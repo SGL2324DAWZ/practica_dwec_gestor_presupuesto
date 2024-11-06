589e338a864df47c812e39542b8a02e176c8ad31
SGL2324DAWZ <selgislop@@alu.edu.gva.es>
2024-11-06 19:46:00 +0100

    actualizarValor
    realizacion de la funcion actualizarValor

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -49,6 +49,12 @@ function actualizarDescripcion(descripcionNueva){
     this.descripcion = descripcionNueva;
 }
 
+function actualizarValor(valorNuevo) {
+    if (typeof valorNuevo === "number" && valor >= 0) {
+        this.valor = valorNuevo;
+    }
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
